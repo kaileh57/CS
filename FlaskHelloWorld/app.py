eb453e1658b378e694180a2742bf37929e03fdb5
kaileh57 <kellenhe@hotmail.com>
2025-09-08 15:05:59 -0700

    day 1

---
+++ FlaskHelloWorld/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__) #this has 2 underscores on each side
+
+@app.route('/')
+def hello_world():
+  return 'Hello World asd!'
